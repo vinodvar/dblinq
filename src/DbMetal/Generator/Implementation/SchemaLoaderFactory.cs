vinodvar/dblinq

commit 72a63d1a4048c0af88f13c62aec80455b98d5be0

Make DbMetal fail a little more gracefully if no config is present
git-svn-id: http://dblinq2007.googlecode.com/svn/trunk@1221 33146b6e-df29-0410-a1ca-39be37fb9416

src/DbMetal/Generator/Implementation/SchemaLoaderFactory.cs
@@ -234,7 +234,7 @@ namespace DbMetal.Generator.Implementation
 
             ProvidersSection.ProviderElement element;
             string errorMsg;
-            if (!configuration.Providers.TryGetProvider(provider, out element, out errorMsg))
+            if (configuration == null || !configuration.Providers.TryGetProvider(provider, out element, out errorMsg))
             {
                 Output.WriteErrorLine(Log, "Failed to load provider {0} : {1}", provider, errorMsg);
                 throw new ApplicationException("Failed to load provider " + provider);

commit 4c087dfd974819b8a3cc548bb0645278842b38ea

Fix compilation error introduced in r1221.

src/DbMetal/Generator/Implementation/SchemaLoaderFactory.cs
@@ -233,7 +233,7 @@ namespace DbMetal.Generator.Implementation
             var configuration = (ProvidersSection)ConfigurationManager.GetSection("providers");
 
             ProvidersSection.ProviderElement element;
-            string errorMsg;
+            string errorMsg = null;
             if (configuration == null || !configuration.Providers.TryGetProvider(provider, out element, out errorMsg))
             {
                 Output.WriteErrorLine(Log, "Failed to load provider {0} : {1}", provider, errorMsg);